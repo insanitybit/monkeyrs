3f697c4f7ae8c622a05b2a4b9baa18026a046441
insanitybit <insanitybit@gmail.com>
2017-01-22 15:13:05 -0500

    Reenable parser.

--- src/token.rs
+++ src/token.rs
@@ -1,6 +1,6 @@
 #![allow(non_camel_case_types)]
 
-// use parser::Precedence;
+use parser::Precedence;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Token<'a> {
@@ -45,19 +45,20 @@ pub enum Token<'a> {
     IF,
     ELSE,
 }
-// impl<'a> Token<'a> {
-//     pub fn get_precedence(&self) -> Precedence {
-//         match *self {
-//             Token::EQ => Precedence::Equals,
-//             Token::NOT_EQ => Precedence::Equals,
-//             Token::GT => Precedence::LessGreater,
-//             Token::LT => Precedence::LessGreater,
-//             Token::PLUS => Precedence::Sum,
-//             Token::MINUS => Precedence::Sum,
-//             Token::SLASH => Precedence::Product,
-//             Token::ASTERISK => Precedence::Product,
-//             Token::LPAREN => Precedence::Call,
-//             _ => Precedence::Lowest,
-//         }
-//     }
-// }
+
+impl<'a> Token<'a> {
+    pub fn get_precedence(&self) -> Precedence {
+        match *self {
+            Token::EQ => Precedence::Equals,
+            Token::NOT_EQ => Precedence::Equals,
+            Token::GT => Precedence::LessGreater,
+            Token::LT => Precedence::LessGreater,
+            Token::PLUS => Precedence::Sum,
+            Token::MINUS => Precedence::Sum,
+            Token::SLASH => Precedence::Product,
+            Token::ASTERISK => Precedence::Product,
+            Token::LPAREN => Precedence::Call,
+            _ => Precedence::Lowest,
+        }
+    }
+}
